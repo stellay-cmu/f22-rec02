e6f33767fdb79667c9f880b2f999ce4365fd5946
stellay-cmu <79229844+stellay-cmu@users.noreply.github.com>
2023-01-25 11:20:53 -0500

    revisions to dining place file

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,7 +1,8 @@
 import { newLine } from "./line";
 import { newPolarPoint } from "./points/polarPoint";
 
-const point1: Point = newPolarPoint(2, 0);
+
+const point1: Point = cartesianPoint(2, 0);
 const point2: Point = newPolarPoint(4, Math.PI);
 
 const line = newLine(point1, point2);
